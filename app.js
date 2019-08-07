dee8e3257617bc0431b554713948b5a26c3560de
Peter Gaspar <petihappygaspar@gmail.com>
2019-08-07 14:53:39 +0300

    - app.js, added style, stripe1.png

---
+++ app.js
@@ -0,0 +1,8 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.5,
+    image1: '1.jpg',
+    image2: '2.jpg',
+    angle: Math.PI / 4,
+    displacementImage: 'stripe1.png'
+});
